40ce1993b04a3e53a6e2dbda37d9ecfd32a7c563
20106050083 <agungrashif009@gmail.com>
2024-04-25 21:02:05 +0700

    commit

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const cubes = require('./cube');
-const blocks = require('./block');
-const prismTriangles = require('./prismTriangle');
+// const cubes = require('./cube');
+// const blocks = require('./block');
+// const prismTriangles = require('./prismTriangle');
 
-const side = 5;
-const volumeCube = cubes.volumeCube(side);
-console.log(`Cube Volume: ${volumeCube}`);
+// const side = 5;
+// const volumeCube = cubes.volumeCube(side);
+// console.log(`Cube Volume: ${volumeCube}`);
 
-const os = require('os')
-const fs = require('fs')
-
-
-fs.writeFileSync('./result.txt', `volume: ${volumeCube}`,'utf-8')
+// const os = require('os')
+// const fs = require('fs')
 
 
-const data = fs.readFileSync('./result.txt', 'utf-8')
-console.log(data)
+
+// fs.writeFileSync('./result.txt', `volume: ${volumeCube}`,'utf-8')
+
+
+// const data = fs.readFileSync('./result.txt', 'utf-8')
+// console.log(data)
+
+
+const { writeFiles, readFiles } = require('./people');
+
+writeFiles(); // You need to pass the 'people' array here
+readFiles();
